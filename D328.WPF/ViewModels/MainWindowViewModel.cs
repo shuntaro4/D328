cb776912b4ca739edf112fff20e07b70559e8ea5
shuntaro4 <shuntaro4@gmail.com>
2019-04-15 21:25:25 +0900

    add AudioData List.

--- D328.WPF/ViewModels/MainWindowViewModel.cs
+++ D328.WPF/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using D328.Platform;
+﻿using D328.Domain.Model;
+using D328.Platform;
 using D328.WPF.Platform;
 using D328.WPF.Repository;
 using NAudio.CoreAudioApi;
@@ -43,6 +44,14 @@ namespace D328.WPF.ViewModels
             set => SetProperty(ref _peek, value);
         }
 
+        private ObservableCollection<D328Record> _recordList;
+
+        public ObservableCollection<D328Record> RecordList
+        {
+            get => _recordList;
+            set => SetProperty(ref _recordList, value);
+        }
+
         public DelegateCommand RecordingStartCommand { get; }
 
         public DelegateCommand RecordingStopCommand { get; }
@@ -59,6 +68,7 @@ namespace D328.WPF.ViewModels
             var audioDevices = AudioDeviceService.GetInputAudioDevices();
             AudioDevices = new ObservableCollection<MMDevice>(audioDevices);
             SelectedAudioDevice = AudioDevices.FirstOrDefault(c => c.ID == AudioDeviceService.GetDefaultInputAudioDevice()?.ID);
+            RecordList = new ObservableCollection<D328Record>();
 
             RecordingStartCommand = new DelegateCommand(RecordingStartCommandExecute);
             RecordingStopCommand = new DelegateCommand(RecordingStopCommandExecute);
@@ -95,6 +105,7 @@ namespace D328.WPF.ViewModels
         private void RecordingStopCommandExecute()
         {
             AudioRecorder?.Stop();
+            RecordList.Add(AudioRecorder.GetRecordData());
 
             RecordingReadyCommandExecute();
         }
