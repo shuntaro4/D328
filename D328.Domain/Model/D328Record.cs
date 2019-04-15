e8ed4d0abc65e9f34bef86cb7afd353985b4706d
shuntaro4 <shuntaro4@gmail.com>
2019-04-15 20:22:46 +0900

    add D328.Domain.D328Record

---
+++ D328.Domain/Model/D328Record.cs
@@ -0,0 +1,14 @@
+﻿namespace D328.Domain.Model
+{
+    public class D328Record
+    {
+        public int Id { get; } = -1;
+
+        public string AudioPath { get; } = "";
+
+        public D328Record(string audioPath)
+        {
+            AudioPath = audioPath;
+        }
+    }
+}
