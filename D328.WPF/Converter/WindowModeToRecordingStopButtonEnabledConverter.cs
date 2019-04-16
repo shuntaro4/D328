167ddedd0d047c38bb93f01906ade5cce303e5a5
shuntaro4 <shuntaro4@gmail.com>
2019-04-17 00:01:58 +0900

    add the recording stop button enabled converter.

---
+++ D328.WPF/Converter/WindowModeToRecordingStopButtonEnabledConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using static D328.WPF.ViewModels.MainWindowViewModel;
+
+namespace D328.WPF.Converter
+{
+    public class WindowModeToRecordingStopButtonEnabledConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var windowMode = value as MainWindowMode?;
+            if (windowMode == null)
+            {
+                return false;
+            }
+            return windowMode == MainWindowMode.Recording || windowMode == MainWindowMode.Pause;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
