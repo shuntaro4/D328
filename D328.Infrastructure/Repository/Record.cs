1a8bb4f0840837639009514d302751641f3147b0
shuntaro4 <shuntaro4@gmail.com>
2019-04-22 23:28:20 +0900

    add the realm package.

---
+++ D328.Infrastructure/Repository/Record.cs
@@ -0,0 +1,11 @@
+﻿using Realms;
+
+namespace D328.Infrastructure.Repository
+{
+    public class Record : RealmObject
+    {
+        public int Id { get; set; }
+
+        public string AudioPath { get; set; }
+    }
+}
