6ca9975a30f8085aef785af74061bc8203c91b31
shuntaro4 <shuntaro4@gmail.com>
2019-04-23 19:24:39 +0900

    add the method to save record object.

--- D328.Infrastructure/Repository/Record.cs
+++ D328.Infrastructure/Repository/Record.cs
@@ -7,5 +7,14 @@ namespace D328.Infrastructure.Repository
         public int Id { get; set; }
 
         public string AudioPath { get; set; }
+
+        public void Save()
+        {
+            var realm = Realm.GetInstance();
+            realm.Write(() =>
+            {
+                realm.Add(this);
+            });
+        }
     }
 }
