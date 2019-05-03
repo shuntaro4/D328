e5e6fadda06662e0c82a1d4649e5073c68861252
shuntaro4 <shuntaro4@gmail.com>
2019-05-03 14:36:36 +0900

    add LineObject.

---
+++ D328.Infrastructure/LineObject.cs
@@ -0,0 +1,17 @@
+﻿using Realms;
+
+namespace D328.Repository
+{
+    public class LineObject : RealmObject
+    {
+        public int Id { get; set; }
+
+        public int SortNumber { get; set; }
+
+        public string AudioPath { get; set; }
+
+        public LineObject()
+        {
+        }
+    }
+}
