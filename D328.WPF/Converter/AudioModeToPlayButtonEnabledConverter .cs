54d107596ff19db61a379f23378f61821e04b68b
shuntaro4 <shuntaro4@gmail.com>
2019-05-24 00:57:53 +0900

    add AudioModeToPlayBUttonEnabledConverter.

---
+++ D328.WPF/Converter/AudioModeToPlayButtonEnabledConverter .cs
@@ -0,0 +1,25 @@
+﻿using D328.Domain.Enum;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace D328.WPF.Converter
+{
+    public class AudioModeToPlayButtonEnabledConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var audioMode = value as AudioMode?;
+            if (audioMode == null)
+            {
+                return false;
+            }
+            return audioMode == AudioMode.Pause || audioMode == AudioMode.Normal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
