b2d9d081d976ec41d4a3c8b9bea0381a3968dc0e
shuntaro4 <shuntaro4@gmail.com>
2019-08-29 10:57:13 +0900

    add D328.Audio.UWP.AudioDeviceService class.

---
+++ D328.Audio.Uwp/AudioDeviceService.cs
@@ -0,0 +1,33 @@
+﻿using D328.Application.Services;
+using D328.Domain;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Windows.Devices.Enumeration;
+
+namespace D328.Audio.UWP
+{
+    public class AudioDeviceService : IAudioDeviceService
+    {
+        public List<AudioDevice> GetInputAudioDevices()
+        {
+            var devices = DeviceInformation.FindAllAsync(DeviceClass.AudioCapture).GetResults();
+            return devices.Select(x => AudioDevice.CreateNewDevice(x.Id, x.Name)).ToList();
+        }
+
+        public List<AudioDevice> GetOutputAudioDevices()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public AudioDevice GetSelectedInputAudioDevice(ObservableCollection<AudioDevice> devices)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public AudioDevice GetSelectedOutputAudioDevice(ObservableCollection<AudioDevice> devices)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
