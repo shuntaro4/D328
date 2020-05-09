0e4acd6bcf2c7460f28a3948f3dd1df463eee024
shuntaro4 <shuntaro4@gmail.com>
2020-05-09 18:32:01 +0900

    add UserName class.

---
+++ D328.MultiRecording/Domain/UserName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace D328.MultiRecording.Domain
+{
+    public class UserName
+    {
+        public string Value { get; private set; }
+
+        public UserName(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException();
+            }
+
+            Value = value;
+        }
+    }
+}
