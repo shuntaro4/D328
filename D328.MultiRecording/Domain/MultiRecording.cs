2685f6b04ff097169285022dc6fa7423fffe0047
shuntaro4 <shuntaro4@gmail.com>
2020-05-09 18:06:51 +0900

    add Title Property to MultiRecording.

--- D328.MultiRecording/Domain/MultiRecording.cs
+++ D328.MultiRecording/Domain/MultiRecording.cs
@@ -1,6 +1,14 @@
-﻿namespace D328.MultiRecording.Domain
+﻿using System;
+
+namespace D328.MultiRecording.Domain
 {
     public class MultiRecording
     {
+        public Title Title { get; private set; }
+
+        public MultiRecording(Title title)
+        {
+            Title = title ?? throw new ArgumentException();
+        }
     }
 }
