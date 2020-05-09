b01f13140a9dcb9d75960967609d860f9890f9eb
shuntaro4 <shuntaro4@gmail.com>
2020-05-09 18:17:06 +0900

    add User Property to Recording.

--- D328.MultiRecording/Domain/Recording.cs
+++ D328.MultiRecording/Domain/Recording.cs
@@ -1,6 +1,14 @@
-﻿namespace D328.MultiRecording.Domain
+﻿using System;
+
+namespace D328.MultiRecording.Domain
 {
     public class Recording
     {
+        public User User { get; private set; }
+
+        public Recording(User user)
+        {
+            User = user ?? throw new ArgumentException();
+        }
     }
 }
