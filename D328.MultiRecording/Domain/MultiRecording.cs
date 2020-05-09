abfe93ae3b0185f9c3c2b6eb0d1ded7c2936df4c
shuntaro4 <shuntaro4@gmail.com>
2020-05-09 18:26:24 +0900

    add MultiRecordingId property to MultiRecording.

--- D328.MultiRecording/Domain/MultiRecording.cs
+++ D328.MultiRecording/Domain/MultiRecording.cs
@@ -6,14 +6,22 @@ namespace D328.MultiRecording.Domain
 {
     public class MultiRecording
     {
+        public MultiRecordingId Id { get; private set; }
         public Title Title { get; private set; }
         public ReadOnlyCollection<Recording> Recordings => recordings.AsReadOnly();
 
         private List<Recording> recordings = new List<Recording>();
 
-        public MultiRecording(Title title)
+        public MultiRecording(MultiRecordingId id, Title title)
         {
+            Id = id ?? throw new ArgumentException();
             Title = title ?? throw new ArgumentException();
+        }
+
+        public static MultiRecording CreateNew(Title title)
+        {
+            var id = MultiRecordingId.CreateNew();
+            return new MultiRecording(id, title);
         }
     }
 }
