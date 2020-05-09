0aa2850b0df4a99b2397d54778082cc73a26e2f1
shuntaro4 <shuntaro4@gmail.com>
2020-05-09 18:28:35 +0900

    add RecordingId property to Recording.

--- D328.MultiRecording/Domain/Recording.cs
+++ D328.MultiRecording/Domain/Recording.cs
@@ -4,11 +4,19 @@ namespace D328.MultiRecording.Domain
 {
     public class Recording
     {
+        public RecordingId Id { get; private set; }
         public User User { get; private set; }
 
-        public Recording(User user)
+        public Recording(RecordingId id, User user)
         {
+            Id = id ?? throw new ArgumentException();
             User = user ?? throw new ArgumentException();
+        }
+
+        public static Recording CreateNew(User user)
+        {
+            var id = RecordingId.CreateNew();
+            return new Recording(id, user);
         }
     }
 }
