bd6a5f7f4e531828bd01874eff5c73e2c7211c3b
shuntaro4 <shuntaro4@gmail.com>
2020-05-09 18:32:57 +0900

    add UserName property to User.

--- D328.MultiRecording/Domain/User.cs
+++ D328.MultiRecording/Domain/User.cs
@@ -1,6 +1,14 @@
-﻿namespace D328.MultiRecording.Domain
+﻿using System;
+
+namespace D328.MultiRecording.Domain
 {
     public class User
     {
+        public UserName Name { get; private set; }
+
+        public User(UserName name)
+        {
+            Name = name ?? throw new ArgumentException();
+        }
     }
 }
