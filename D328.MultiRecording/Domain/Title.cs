0e58432cd300578dc345cb2aae558d0ffa760e12
shuntaro4 <shuntaro4@gmail.com>
2020-05-09 17:29:56 +0900

    add Title class.

---
+++ D328.MultiRecording/Domain/Title.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace D328.MultiRecording.Domain
+{
+    public class Title
+    {
+        public string Value { get; private set; }
+
+        public Title(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException();
+            }
+
+            Value = value;
+        }
+    }
+}
