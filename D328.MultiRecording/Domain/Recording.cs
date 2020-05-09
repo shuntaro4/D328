294bd0b572c8107a4c45b17060a340942d23857d
shuntaro4 <shuntaro4@gmail.com>
2020-05-09 18:46:28 +0900

    add AudioFile property to Recording.

--- D328.MultiRecording/Domain/Recording.cs
+++ D328.MultiRecording/Domain/Recording.cs
@@ -6,17 +6,20 @@ namespace D328.MultiRecording.Domain
     {
         public RecordingId Id { get; private set; }
         public User User { get; private set; }
+        public AudioFile AudioFile { get; private set; }
 
-        public Recording(RecordingId id, User user)
+
+        public Recording(RecordingId id, User user, AudioFile audioFile)
         {
             Id = id ?? throw new ArgumentException();
             User = user ?? throw new ArgumentException();
+            AudioFile = audioFile ?? throw new ArgumentException();
         }
 
-        public static Recording CreateNew(User user)
+        public static Recording CreateNew(User user, AudioFile audioFile)
         {
             var id = RecordingId.CreateNew();
-            return new Recording(id, user);
+            return new Recording(id, user, audioFile);
         }
     }
 }
