29ec6cee7f0a73cff3cbc5eab684a61e068d7a4f
shuntaro4 <shuntaro4@gmail.com>
2020-05-09 18:21:50 +0900

    add MultiRecordingId class.

---
+++ D328.MultiRecording/Domain/MultiRecordingId.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace D328.MultiRecording.Domain
+{
+    public class MultiRecordingId
+    {
+        public long Value { get; private set; }
+
+        public MultiRecordingId(long value)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException();
+            }
+
+            Value = value;
+        }
+
+        public static MultiRecordingId CreateNew()
+        {
+            return new MultiRecordingId(DateTime.Now.Ticks);
+        }
+    }
+}
