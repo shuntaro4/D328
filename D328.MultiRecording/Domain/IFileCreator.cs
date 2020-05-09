664a9ee8e0cb724efba335f521d77c08c7abaea6
shuntaro4 <shuntaro4@gmail.com>
2020-05-09 20:29:43 +0900

    add FileCreator class.

---
+++ D328.MultiRecording/Domain/IFileCreator.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace D328.MultiRecording.Domain
+{
+    public interface IFileCreator
+    {
+        Task<StorageFile> CreateAudioFileAsync();
+    }
+}
---
+++ D328.MultiRecording/Infrastructure/FileCreator.cs
@@ -0,0 +1,20 @@
+﻿using D328.MultiRecording.Domain;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace D328.MultiRecording.Infrastructure
+{
+    public class FileCreator : IFileCreator
+    {
+        public async Task<StorageFile> CreateAudioFileAsync()
+        {
+            var folderPath = @$"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\D328\AudioFiles";
+            Directory.CreateDirectory(folderPath);
+            var folder = await StorageFolder.GetFolderFromPathAsync(folderPath);
+            var fileName = $"{DateTime.Now:yyyy-MM-dd-HHmmssfffffff}.mp3";
+            return await folder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+        }
+    }
+}
