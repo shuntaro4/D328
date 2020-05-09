6a939a685877fa5e1965f7ef824043210b5fa5b1
shuntaro4 <shuntaro4@gmail.com>
2020-05-09 18:50:51 +0900

    add IsMute class.

---
+++ D328.MultiRecording/Domain/IsMute.cs
@@ -0,0 +1,12 @@
+﻿namespace D328.MultiRecording.Domain
+{
+    public class IsMute
+    {
+        public bool Value { get; private set; }
+
+        public IsMute(bool value)
+        {
+            Value = value;
+        }
+    }
+}
