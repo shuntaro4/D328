d8364ba44d765e065d5eac6036d8c646d27f5672
shuntaro4 <shuntaro4@gmail.com>
2020-05-09 20:39:59 +0900

    add DIContainer class.

---
+++ D328.DI/DIContainer.cs
@@ -0,0 +1,30 @@
+﻿using D328.MultiRecording.Domain;
+using D328.MultiRecording.Infrastructure;
+using D328.MultiRecording.UseCase;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace D328.DI
+{
+    public class DIContainer
+    {
+        public static DIContainer Instance { get; } = new DIContainer();
+
+        private ServiceProvider serviceProvider;
+
+        public DIContainer()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            // MultiRecording
+            serviceCollection.AddSingleton<IFileCreator, FileCreator>();
+            serviceCollection.AddSingleton<IRecordingUseCase, RecordingUseCase>();
+
+            serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        public T Resolve<T>()
+        {
+            return serviceProvider.GetRequiredService<T>();
+        }
+    }
+}
