3ffa73c3a12faaece94bd0f587ab706aeeaf37b6
shuntaro4 <shuntaro4@gmail.com>
2020-05-09 18:13:28 +0900

    add Recording Property to MultiRecording.

--- D328.MultiRecording/Domain/MultiRecording.cs
+++ D328.MultiRecording/Domain/MultiRecording.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace D328.MultiRecording.Domain
 {
     public class MultiRecording
     {
         public Title Title { get; private set; }
+        public ReadOnlyCollection<Recording> Recordings => recordings.AsReadOnly();
+
+        private List<Recording> recordings = new List<Recording>();
 
         public MultiRecording(Title title)
         {
