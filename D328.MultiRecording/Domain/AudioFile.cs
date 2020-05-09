6ef20065568716a8d405f72f33b62df15713be95
shuntaro4 <shuntaro4@gmail.com>
2020-05-09 18:42:36 +0900

    add AudioFile class.

---
+++ D328.MultiRecording/Domain/AudioFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace D328.MultiRecording.Domain
+{
+    public class AudioFile
+    {
+        public string Path { get; }
+
+        public AudioFile(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException();
+            }
+
+            Path = path;
+        }
+    }
+}
