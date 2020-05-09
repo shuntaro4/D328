4774edad062c9d05af15ac1905771ea8197bcbf0
shuntaro4 <shuntaro4@gmail.com>
2020-05-09 18:53:34 +0900

    add IsMute property to Recording.

--- D328.MultiRecording/Domain/Recording.cs
+++ D328.MultiRecording/Domain/Recording.cs
@@ -7,19 +7,22 @@ namespace D328.MultiRecording.Domain
         public RecordingId Id { get; private set; }
         public User User { get; private set; }
         public AudioFile AudioFile { get; private set; }
+        public IsMute IsMute { get; private set; }
 
 
-        public Recording(RecordingId id, User user, AudioFile audioFile)
+        public Recording(RecordingId id, User user, AudioFile audioFile, IsMute isMute)
         {
             Id = id ?? throw new ArgumentException();
             User = user ?? throw new ArgumentException();
             AudioFile = audioFile ?? throw new ArgumentException();
+            isMute = isMute ?? throw new ArgumentException();
         }
 
         public static Recording CreateNew(User user, AudioFile audioFile)
         {
             var id = RecordingId.CreateNew();
-            return new Recording(id, user, audioFile);
+            var isMute = new IsMute(false);
+            return new Recording(id, user, audioFile, isMute);
         }
     }
 }
