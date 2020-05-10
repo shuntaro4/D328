1c70bf33cbbead92c796488f7ae7f0086bb9b1dc
shuntaro4 <shuntaro4@gmail.com>
2020-05-10 15:11:04 +0900

    add AudioDevice class.

---
+++ D328.MultiRecording/Domain/AudioDevice.cs
@@ -0,0 +1,19 @@
+﻿using Windows.Devices.Enumeration;
+
+namespace D328.MultiRecording.Domain
+{
+    public class AudioDevice
+    {
+        public string Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public DeviceInformation Value { get; private set; }
+
+        public AudioDevice(DeviceInformation deviceInformation)
+        {
+            Id = deviceInformation.Id;
+            Name = deviceInformation.Name;
+        }
+    }
+}
