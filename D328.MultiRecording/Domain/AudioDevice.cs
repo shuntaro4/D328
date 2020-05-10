2f7f40c50fb63b129ab06fec3816c0691cdba4cc
shuntaro4 <shuntaro4@gmail.com>
2020-05-10 16:01:47 +0900

    fix to init Value property.

--- D328.MultiRecording/Domain/AudioDevice.cs
+++ D328.MultiRecording/Domain/AudioDevice.cs
@@ -14,6 +14,7 @@ namespace D328.MultiRecording.Domain
         {
             Id = deviceInformation.Id;
             Name = deviceInformation.Name;
+            Value = deviceInformation;
         }
     }
 }
