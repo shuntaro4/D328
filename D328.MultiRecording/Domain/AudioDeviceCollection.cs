a9326a1578ef9092954dfc9f18798bcc105e85de
shuntaro4 <shuntaro4@gmail.com>
2020-05-10 15:11:13 +0900

    add AudioDeviceCollection class.

---
+++ D328.MultiRecording/Domain/AudioDeviceCollection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Windows.Devices.Enumeration;
+
+namespace D328.MultiRecording.Domain
+{
+    public class AudioDeviceCollection : Collection<AudioDevice>
+    {
+        public AudioDeviceCollection(IList<AudioDevice> list) : base(list) { }
+
+        public AudioDeviceCollection(DeviceInformationCollection deviceInformationCollection)
+        {
+            var list = new List<AudioDevice>();
+            deviceInformationCollection.ToList().ForEach(x => list.Add(new AudioDevice(x)));
+        }
+    }
+}
