bd1bb3406e34c580cd2b615270ecea2f1275560f
shuntaro4 <shuntaro4@gmail.com>
2020-05-10 15:20:28 +0900

    add NotifyPropertyChangedBase class.

---
+++ D328.Presentation/Framework/NotifyPropertyChangedBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace D328.Presentation.Framework
+{
+    public class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
