3cadd4eba487df7755696afed9a12276172ac4c2
shuntaro4 <shuntaro4@gmail.com>
2020-05-11 23:41:53 +0900

    add InverseBooleanConverter class.

---
+++ D328.Presentation/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace D328.Presentation.Converters
+{
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var target = value as bool? ?? false;
+            return !target;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var target = value as bool? ?? false;
+            return !target;
+        }
+    }
+}
